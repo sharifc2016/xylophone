e3caf2a462ba5caa7f8cb696b8602bf6d10d22ca
sharifc2016 <sharifc2016@gmail.com>
2021-09-05 18:45:53 +0600

    add button and audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MyApp(),
+  );
 }
 
-class MyApp extends StatefulWidget {
+Expanded buildKey(Color color, int number){
+  return Expanded(child: TextButton(
+    onPressed: () {
+      final player = AudioCache();
+      player.play('note$number.wav');
+    },
+    child: Container(
+      color: color,
+    ),
+  ),);
+}
+
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                buildKey(Colors.red, 1),
+                buildKey(Colors.green, 2),
+                buildKey(Colors.blue, 3),
+                buildKey(Colors.yellow, 4),
+                buildKey(Colors.orange, 5),
+                buildKey(Colors.black, 6),
+                buildKey(Colors.teal, 7),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
